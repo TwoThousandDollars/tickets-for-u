b36bc0067b7f793cda4bdfb2a0d67371cf25360a
TwoThousandDollars <bbrooks1@harding.edu>
2021-03-21 14:58:07 -0500

    created game show page and added links to the team's specific page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,21 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: %i[ show edit update destroy ]
+
   def index
     @games = Game.where(season: 2021)
   end
 
   def show
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def game_params
+    params.require(:game).permit(:code, :city, :mascot, :division_id)
   end
 end
